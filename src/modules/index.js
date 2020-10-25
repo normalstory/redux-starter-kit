f0400d623d83f8b28f3dccc9b92156b0974fb688
thinknormal <nornalstory@gmail.com>
2020-10-25 19:40:34 +0900

    02. 라이브러리 안에 내장된 리듀서를 루트 리듀서에 이식

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,12 @@
+//02. 라이브러리 안에 내장된 리듀서를 루트 리듀서에 이식 
 import { combineReducers } from 'redux';
 import counter from './counter';
 
+import post from './post';
+import {penderReducer} from 'redux-pender';
+
 export default combineReducers({
-    counter
+    counter,
+    post,
+    pender: penderReducer
 });
