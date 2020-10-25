0f452c1fc9a80cb73f9e234ee2e07a94a169f40f
thinknormal <nornalstory@gmail.com>
2020-10-25 19:24:12 +0900

    01. 스토어에 pender 미들웨어 적용

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+//01. 스토어에 pender 미들웨어 적용 
+
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
-const store = createStore(modules)
+import {createLogger} from 'redux-logger';
+import pendingMiddleware from 'redux-pender';
+
+const logger = createLogger();
+const store = createStore(modules, applyMiddleware(logger, pendingMiddleware()))
 
 export default store;
